02ed5d124f228302643c8b65939f8ef63c021081
Asad Waseem <asad.waseem@fu-berlin.de>
2025-05-11 09:11:29 +0200

    Staff_Updated

---
+++ staff/people/Asad Waseem.R
@@ -0,0 +1,24 @@
+---
+title: "Asad Waseem"
+author: "Remote sensing & geoinformatics group"
+date: "12-05-2025"
+categories: [RESEDA]
+image: "Asad.jpg"
+sidebar: false
+---
+  
+  **Institute of Geographical Sciences**\
+
+Remote Sensing and Geoinformatics\
+Student collaborator
+
+::: panel-tabset
+
+#### Contact
+
+**Address:** Malteserstraße 74-100 Room H048 12249 Berlin \
+**Telephone:** +49 157 8222 3656 \
+**Email:** asad.waseem[at]fu-berlin.de \
+**Office hours:** By appointment via e-mail. \
+
+:::
